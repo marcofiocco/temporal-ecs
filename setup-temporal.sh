8772bc53740141e2b9556fe1ce9096230e519bd7
Marco Fiocco <marco.fiocco@autodesk.com>
2022-12-16 17:22:34 +0800

    Some fixes

--- setup-temporal.sh
+++ setup-temporal.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
 [ -z "$1" ] && echo "No ENV Name argument supplied" && exit 1
+[ -z "$2" ] && echo "No Permission Boundary argument supplied" && exit 1
+[ -z "$3" ] && echo "No Resource Tag argument supplied" && exit 1
 
 Env=$1
+PermissionBoundary=$2
+ResourceTag=$3
+
 AppName="temporal-app"
 SvcName="temporal-svc"
 UISvcName="temporal-web-svc"
@@ -27,14 +32,14 @@ sed -i -r "s/someUiName/$UISvcName/" copilot/$UISvcName/manifest.yml
 sed -i -r "s/someTemporalAddress/$TemporalAddress/" copilot/$UISvcName/manifest.yml
 rm copilot/$UISvcName/manifest.yml-r
 
-copilot app init --permissions-boundary ADSK-Boundary --resource-tags adsk:moniker="RESGAD-C-UW2"
+copilot app init --permissions-boundary $PermissionBoundary --resource-tags $ResourceTag
 
 copilot env init --name $Env
 copilot env deploy --name $Env # this creates the ECS cluster, security group and service discovery (Cloud Map)
 
-copilot svc init -a "$AppName" -t "Load Balanced Web Service" -n "$SvcName" # this creates the load balancer, ECS service and addons
+copilot svc init -a "$AppName" -t "Load Balanced Web Service" -n "$SvcName" # this creates a public applcation or network load balancer, ECS service and addons
 
-copilot svc init -a "$AppName" -t "Load Balanced Web Service" -n "$UISvcName" # this creates the load balancer, ECS service and addons
+copilot svc init -a "$AppName" -t "Load Balanced Web Service" -n "$UISvcName" # this creates a public applcation or network load balancer, ECS service and addons
 
 echo "Deploying $SvcName"
 copilot svc deploy --name "$SvcName" -e "$Env" # this actually instantiates the resources
